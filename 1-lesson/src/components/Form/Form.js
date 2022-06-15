6f7ace4edcc1240abb4abefb2832da5880c7c668
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2022-06-15 22:39:09 +0300

    feat:2-lesson setState isue

--- 1-lesson/src/components/Form/Form.js
+++ 1-lesson/src/components/Form/Form.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import formStyles from './Form.module.css';
 import Input from '../Form/Input/Input.js';
-import Button from './Buttons/Button';
+// import Button from './Buttons/Button';
 import Textarea from './Textarea/Textarea.js';
 
 
 class Form extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {
-	// 		counter: 2
-	// 	};
-	// }
+	constructor(props) {
+		super(props);
+		this.inputNameValue = React.createRef()
+	}
 
-	// clearFunction = () => {
-	// 	console.log(`rrt`)
-	// }
+	onClikl = () => {
+		const mainState = {
+			textInputValue: this.inputNameValue.current.state.textInputValue,
+			telInputValue: this.inputNameValue.current.state.telInputValue,
+			urlInputValue: this.inputNameValue.current.state.urlInputValue
+		}
+		console.log(this.inputNameValue.current.state)
+
+	}
 
 	render() {
 		return (
 			<form action="#" className={formStyles.main} method="post">
 				<h2>Survey creation</h2>
-				<Input name="Name" />
-				<Input name="Last name" />
+				<Input name="Name" ref={this.inputNameValue} />
+				<Input name="Last name" ref={this.inputNameValue} />
 				<Input name="Birthday" type="date" />
-				<Input name="Phone number" type="tel" symbolLength={12} />
-				<Input name="Web-page" type="url" />
+				<Input name="Phone number" type="tel" symbolLength={12} ref={this.inputNameValue} />
+				<Input name="Web-page" type="url" ref={this.inputNameValue} />
 				<Textarea name="Elevator pitch" type="textarea" rows='7' symbolLength={600} />
 				<Textarea name="Hurd skills" type='textarea' rows='7' symbolLength={600} />
 				<Textarea name="Your last project" type='textarea' rows='7' symbolLength={600} />
 				<div className={formStyles.button_wrapper}>
-					<Button name="Clear" color="red" type="reset" />
-					<Button name="Save" type="button" />
+					<button className={formStyles.btn} style={{ backgroundColor: 'red' }} type="reset" >Clear</button>
+					<button onClick={this.onClikl} className={formStyles.btn} type="button">Save</button>
 				</div>
 			</form>
 		);
--- 1-lesson/src/components/Form/Input/Input.js
+++ 1-lesson/src/components/Form/Input/Input.js
@@ -6,35 +6,25 @@ class Input extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            value: ''
+            errorValue: '',
+            textInputValue: '',
+            telInputValue: '',
+            urlInputValue: ''
         }
     }
-
-    isEmptyValidator(event) { }
 
     textValidator = (event) => {
         const inputValue = event.target.value.trim();
         const firstSymbol = inputValue.charAt(0);
 
-        // if (parseInt(inputValue)) {
-        //     this.setState({
-        //         value: `You must enter only letters`
-        //     })
-        //     return;
-        // } else {
-        //     this.setState({
-        //         value: ''
-        //     })
-        // }
-
         if (firstSymbol !== firstSymbol.toUpperCase()) {
             this.setState({
-                value: `First symbol should be capital `
+                errorValue: `First symbol should be capital `
             })
             return;
         } else {
             this.setState({
-                value: ''
+                errorValue: ''
             })
         }
     }
@@ -44,34 +34,26 @@ class Input extends React.Component {
 
         if (Number.isNaN(+telInput)) {
             this.setState({
-                value: `you must enter only numbers `
+                errorValue: `you must enter only numbers `
             })
             return;
         } else {
             this.setState({
-                value: ``
+                errorValue: ``
             })
         }
-
-        // if (event.target.value.length !== 12) {
-        //     this.setState({
-        //         value: `you must enter 12 numbers `
-        //     })
-        // } else {
-        //     this.setState({
-        //         value: ``
-        //     })
-        // }
     }
+
     urlValidator = (event) => {
-        const inputTrimValue = event.target.value.trim()
+        const inputTrimValue = event.target.value.trim();
+
         if (!inputTrimValue.startsWith("https://") && inputTrimValue.length !== 0) {
             this.setState({
-                value: `you must enter: "https://" `
+                errorValue: `you must enter: "https://" `
             })
         } else {
             this.setState({
-                value: ``
+                errorValue: ``
             })
         }
     }
@@ -80,13 +62,21 @@ class Input extends React.Component {
         switch (this.props.type) {
             case 'text':
                 this.textValidator(event)
+                this.setState({
+                    textInputValue: event.target.value
+                })
                 break;
-
             case 'tel':
                 this.telValidator(event)
+                this.setState({
+                    telInputValue: event.target.value
+                })
                 break;
             case 'url':
                 this.urlValidator(event)
+                this.setState({
+                    urlInputValue: event.target.value
+                })
 
         }
     }
@@ -103,7 +93,7 @@ class Input extends React.Component {
                     placeholder={this.props.name}
                     rows={this.props.rows}
                 />
-                <div className={inputStyles.warning}>{this.state.value}</div>
+                <div className={inputStyles.warning}>{this.state.errorValue}</div>
             </label>
         )
     }
